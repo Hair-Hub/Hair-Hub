2a595a5ada043355e00d4198e4648165a6608a28
IvoryWatkins <ivory.watkins23@gmail.com>
2024-03-15 13:00:14 -0500

    adding single items and allitems page

--- src/client/components/AllItems.jsx
+++ src/client/components/AllItems.jsx
@@ -1,1 +1,18 @@
-import { useEffect, useState}
+import { useEffect, useState } from 'react'
+import { useNavbar } from 'react-router-dom'
+import { AllItems } from '../API'
+
+
+export default function AllItems() {
+    const navbar = useNavbar()
+    const [items, setItmes] = useState([])
+    const [search, setSearch] = useState('')
+
+    useEffect(() => {
+        try {
+            const Items = await AllItems()
+            console.log('items', Items)
+            setItmes(items)
+        }
+    }
+}
