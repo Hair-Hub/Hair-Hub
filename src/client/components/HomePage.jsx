4f70b51144d108f3cebfe03414b122c8236e847b
richard <ricstewa41@cox.net>
2024-03-15 12:21:40 -0500

    worked on homepage and navbar

---
+++ src/client/components/HomePage.jsx
@@ -0,0 +1,10 @@
+
+
+export default function HomePage() {
+    return <>
+    <div>
+        <h3>Discover New Products and Community Engagement,
+             All While Finding What Works For You</h3>
+    </div>
+    </>
+}
