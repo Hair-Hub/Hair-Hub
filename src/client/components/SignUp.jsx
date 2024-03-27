0ea89f08cbd99633880572b0fc0e6095d19a7661
richard <ricstewa41@cox.net>
2024-03-27 12:52:50 -0500

    final commit

--- src/client/components/SignUp.jsx
+++ src/client/components/SignUp.jsx
@@ -39,16 +39,45 @@ const SignUp = ({ setToken }) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Name</label>
-           <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
-           <label>Email</label>
-           <input type='email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
-           <label>Password</label>
-           <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}></input>
-            <button type="submit">Submit</button>
-        </form>
-    )
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h2>Create an Account</h2>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your name"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    );
+    
 }
 
 export default SignUp
